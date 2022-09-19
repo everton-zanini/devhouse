4147289645a84ba2f4982a20e3d4b637dad8c1a2
Zanini <everton.zanini@signainfo.com.br>
2022-09-19 14:25:23 -0300

    feat: implementado Cancelar Reservas

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -3,6 +3,23 @@ import User from "../models/User";
 import House from "../models/House";
 
 class ReserveController {
+
+    async destroy(req,res){
+        const {reserve_id} = req.body;
+
+        await Reserve.findByIdAndDelete({_id: reserve_id})
+
+        return res.send();
+    }
+
+    async index(req,res){
+        const {user_id} = req.headers;
+
+        const reserves = await Reserve.find({user: user_id}).populate('house').exec();
+
+        return res.json(reserves)
+    }
+
     async store(req, res) {
         const { user_id } = req.headers
         const { house_id } = req.params;
